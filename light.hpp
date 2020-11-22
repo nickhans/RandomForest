6ce53858c2e44a180330ee7067e9c72cebf0bac4
Nick Hanselman <nickhanselman@icloud.com>
2020-11-22 07:50:07 -0700

    basic light class

---
+++ light.hpp
@@ -0,0 +1,35 @@
+#ifndef __LIGHT_HPP__
+#define __LIGHT_HPP__
+
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GLUT/glut.h>
+#endif
+
+#include "util.hpp"
+
+class Light {
+ public:
+  bool on;
+  Light();
+
+  void set_light();
+
+ private:
+  int distance;
+  float emission;
+  float ambient;
+  float diffuse;
+  float specular;
+  int shininess;
+  int azimuth;
+  float elevation;
+
+  float x, z, r;
+
+  void sun();
+};
+
+#endif
